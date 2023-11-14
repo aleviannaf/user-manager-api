76428f107e0f70aeb022ae0119d2b99753bf443a
aleviannaf <alexandrevianna511@gmail.com>
2023-11-14 15:36:47 -0400

    feat: create middleware to verify if id exist

---
+++ src/middlewares/verifyIdExist.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { useRepository } from "../repositories";
+import AppError from "../errors/AppError.error";
+import User from "../entities/User.entity";
+
+export const verifyIdexist = async (req: Request, res: Response, next: NextFunction): Promise<void> =>{
+    const foundUser: User | null = await useRepository.findOneBy({id: Number(req.params.userId)})
+
+    if(!foundUser) {
+        throw new AppError('User not found', 404)
+    }
+
+    res.locals = { ...res.locals, foundUser}
+
+    return next()
+}
