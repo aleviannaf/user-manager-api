cdfdc95bf9211989674eed2a15fcd1b6b0e01599
aleviannaf <alexandrevianna511@gmail.com>
2023-11-14 15:59:19 -0400

    feat: router to update user

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { createUserController, deleteUserController, readUserByIdController, readUserController } from "../controllers/user.controller"
+import { createUserController, deleteUserController, readUserByIdController, readUserController, updateUserController } from "../controllers/user.controller"
 import { verifyIdexist } from "../middlewares/verifyIdExist.middleware"
 
 export const userRouter: Router = Router()
 
 userRouter.post('/', createUserController)
 userRouter.get('/', readUserController)
+userRouter.patch('/:userId', verifyIdexist, updateUserController)
 userRouter.get('/:userId', verifyIdexist, readUserByIdController)
 userRouter.delete('/:userId', verifyIdexist, deleteUserController)
