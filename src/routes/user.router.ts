d7fedb6dd74a245f93a27266f07e952e64f1a10b
aleviannaf <alexandrevianna511@gmail.com>
2023-11-14 15:36:13 -0400

    feat: added middleware to verify id exist

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express"
 import { createUserController, deleteUserController, readUserByIdController, readUserController } from "../controllers/user.controller"
+import { verifyIdexist } from "../middlewares/verifyIdExist.middleware"
 
 export const userRouter: Router = Router()
 
 userRouter.post('/', createUserController)
 userRouter.get('/', readUserController)
-userRouter.get('/:userId', readUserByIdController)
-userRouter.delete('/:userId', deleteUserController)
+userRouter.get('/:userId', verifyIdexist, readUserByIdController)
+userRouter.delete('/:userId', verifyIdexist, deleteUserController)
