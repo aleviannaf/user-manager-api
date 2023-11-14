f0101067e6ca6635e4d4ea7d81cdc06c10ab2ac2
aleviannaf <alexandrevianna511@gmail.com>
2023-11-14 15:37:32 -0400

    feat: create controller to update user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import { createUserService, deleteUserService, readUserByIdService, readUserService } from "../services/user.service"
+import { createUserService, deleteUserService, readUserByIdService, readUserService, upadteUserServive } from "../services/user.service"
 import User from "../entities/User.entity"
 
 export const createUserController = async (req: Request, res: Response): Promise<Response> =>{
@@ -15,13 +15,19 @@ export const readUserController = async (req: Request, res: Response): Promise<R
 } 
 
 export const readUserByIdController = async (req: Request, res: Response): Promise<Response> =>{
-    const user: User = await readUserByIdService(Number(req.params.userId))
+    const user: User = await readUserByIdService(Number(res.locals.foundUser.id))
+
+    return res.status(200).json(user)
+} 
+
+export const updateUserController = async (req: Request, res: Response): Promise<Response> =>{
+    const user: User = await upadteUserServive(res.locals.foundUser, req.body)
 
     return res.status(200).json(user)
 } 
 
 export const deleteUserController = async (req: Request, res: Response): Promise<Response> =>{
-    await deleteUserService(Number(req.params.userId))
+    await deleteUserService(Number(res.locals.foundUser.id))
 
     return res.status(204).send()
 } 
