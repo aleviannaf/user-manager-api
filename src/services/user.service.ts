93fcebb66aa98b6cc5bd605b426e736f137fa570
aleviannaf <alexandrevianna511@gmail.com>
2023-11-16 15:37:24 -0400

    Revert "feat: added pagination in controller"
    This reverts commit 68d5a0ac853cd1bbe133502ecbcffd041bee9517.

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,40 +1,22 @@
 import User from "../entities/User.entity";
-import { Pagination, PaginationParamns } from "../interfaces/pagination.interface";
 import { useRepository } from "../repositories";
 
-export const createUserService = async (data: Omit<User, 'id'>): Promise<User> => {
+export const createUserService = async (data: Omit<User, 'id'>): Promise<User> =>{
     const newUser: User = await useRepository.save(data)
 
     return newUser
 }
 
-export const readUserService = async (
-    {
-        prevPage,
-        nextPage,
-        perPage,
-        page
-    }: PaginationParamns
-): Promise<Pagination> => {
+export const readUserService = async (): Promise<User[]> =>{
+    const users: User[] = await useRepository.find()
 
-    const [users, count] = await useRepository.findAndCount({
-        skip: page, // offset
-        take: perPage, // limit
-    })
-
-
-    return {
-        prevPage: page <= 1 ? null : prevPage,
-        nextPage: count - page <= perPage ? null : nextPage,
-        data: users,
-        count,
-    }
+    return users
 }
 
-export const upadteUserServive = async (user: User, data: Partial<User>): Promise<User> => {
-    return await useRepository.save({ ...user, ...data })
+export const upadteUserServive = async (user: User, data: Partial<User>): Promise<User> =>{
+    return await useRepository.save({...user, ...data})
 }
 
-export const deleteUserService = async (user: User): Promise<void> => {
+export const deleteUserService = async (user: User):Promise<void>=>{
     await useRepository.remove(user)
 }
